a298052f6e459ef1f0cdb5f08f3734e9c7da2031
Brendon Bown <brendon.bown@gmail.com>
2022-09-07 14:21:48 -0600

    Initial commit

---
+++ Token.h
@@ -0,0 +1,39 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include <string>
+
+enum class TokenType {
+    COMMA,
+    PERIOD,
+    Q_MARK,
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    COLON,
+    COLON_DASH,
+    MULTIPLY,
+    ADD,
+    SCHEMES,
+    FACTS,
+    RULES,
+    QUERIES,
+    ID,
+    STRING,
+    COMMENT,
+    EOF,
+    UNDEFINED
+};
+
+struct Token
+{
+TokenType type;
+std::string lexeme;
+int line;
+
+Token(TokenType type, std::string lexeme, int line):
+    type(type),
+    lexeme(lexeme),
+    line(line) {}
+};
+
+#endif // TOKEN_H
+
