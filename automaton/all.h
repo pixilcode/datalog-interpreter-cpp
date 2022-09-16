61c6aceb2aa2c8ffaf999d0de7aa2b88f8407ab1
Brendon Bown <brendon.bown@gmail.com>
2022-09-16 14:01:37 -0600

    Move automatons into 'submodule'

---
+++ automaton/all.h
@@ -0,0 +1,8 @@
+#ifndef PROJECT_1_ALL_H
+#define PROJECT_1_ALL_H
+
+// Include for easier importation of all automatons
+#include "automaton/ColonAutomaton.h"
+#include "automaton/ColonDashAutomaton.h"
+
+#endif //PROJECT_1_ALL_H
