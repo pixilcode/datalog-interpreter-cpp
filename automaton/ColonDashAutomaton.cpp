c22ecfdbce2b987ca04d02d49ef3c5def09e0042
Brendon Bown <brendon.bown@gmail.com>
2022-09-16 14:06:16 -0600

    Add some colondash tests

--- automaton/ColonDashAutomaton.cpp
+++ automaton/ColonDashAutomaton.cpp
@@ -27,11 +27,27 @@ TestResult ColonDashAutomaton::testAutomaton() {
             colonDash.start("abc", 0, 1)
     };
 
-    return test::all({
+    auto hasValueTests = test::all({
         test::assert(results[0].has_value(), "colondash - didn't match colondash"),
         test::assert(!results[1].has_value(), "colondash - matched non-colondash value ':'"),
         test::assert(!results[2].has_value(), "colondash - matched empty string"),
         test::assert(results[3].has_value(), "colondash - didn't match colondash in ':-:'"),
         test::assert(!results[4].has_value(), "colondash - matched non-colondash in 'abc'"),
+    });
+
+    auto resultValue = results[0].value();
+    auto resultToken = resultValue.token;
+
+    auto correctValueTests = test::all({
+        test::assert(resultValue.finalIndex == 1, "colon - didn't advance index"),
+        test::assert(resultValue.finalLine == 1, "colon - incorrect line number"),
+        test::assert(resultToken.type == TokenType::COLON, "colon - incorrect token type"),
+        test::assert(resultToken.lexeme == ":", "colon - incorrect lexeme"),
+        test::assert(resultToken.line == 1, "colon - incorrect line")
+    });
+
+    return test::all({
+        hasValueTests,
+        correctValueTests
     });
 }
