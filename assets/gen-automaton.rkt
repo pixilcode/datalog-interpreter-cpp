1b6e7c5bd4ba2d618f6fa6549fdf3f778fc6def0
Brendon Bown <brendon.bown@gmail.com>
2022-09-19 14:28:12 -0600

    Generate keyword automata

--- assets/gen-automaton.rkt
+++ assets/gen-automaton.rkt
@@ -35,13 +35,32 @@
                           replacements))
 
 
-(for-each
- (lambda (it) (apply generate-single-char-automaton it))
- '(("Colon" ":")
-   ("Comma" ",")
-   ("Period" ".")
-   ("QuestionMark" "?" "Q_MARK")
-   ("LeftParen" "(" "LEFT_PAREN")
-   ("RightParen" ")" "RIGHT_PAREN")
-   ("Multiply" "*")
-   ("Add" "+")))
+(define (generate-all-single-chars) (for-each
+                                     (lambda (it) (apply generate-single-char-automaton it))
+                                     '(("Colon" ":")
+                                       ("Comma" ",")
+                                       ("Period" ".")
+                                       ("QuestionMark" "?" "Q_MARK")
+                                       ("LeftParen" "(" "LEFT_PAREN")
+                                       ("RightParen" ")" "RIGHT_PAREN")
+                                       ("Multiply" "*")
+                                       ("Add" "+"))))
+
+(define (generate-keyword-automaton token-name)
+  (define all-caps-name (string-upcase token-name))
+  (define test-name (string-downcase token-name))
+  (define token-length (number->string (string-length token-name)))
+  (define replacements `(("token-name" ,token-name)
+                         ("token-length" ,token-length)
+                         ("all-caps-name" ,all-caps-name)
+                         ("test-name" ,test-name)))
+
+  (generate-from-template "TemplateKeywordAutomaton.h.template"
+                          (string-append token-name "Automaton.h")
+                          replacements)
+  (generate-from-template "TemplateKeywordAutomaton.cpp.template"
+                          (string-append token-name "Automaton.cpp")
+                          replacements))
+
+(define (generate-all-keywords) (for-each generate-keyword-automaton
+                                          '("Schemes" "Facts" "Rules" "Queries")))
