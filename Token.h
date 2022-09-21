2bee67e6e594aaa573d3a20feb0ab4475a766274
Brendon Bown <brendon.bown@gmail.com>
2022-09-21 13:28:32 -0600

    Add string output to Token type

--- Token.h
+++ Token.h
@@ -2,6 +2,7 @@
 #define TOKEN_H
 #include <string>
 #include <utility>
+#include <iostream>
 
 enum class TokenType {
     COMMA,
@@ -24,6 +25,54 @@ enum class TokenType {
     UNDEFINED
 };
 
+std::string tokenTypeToString(const TokenType& type) {
+    switch (type) {
+        case TokenType::COMMA:
+            return "COMMA";
+        case TokenType::PERIOD:
+            return "PERIOD";
+        case TokenType::Q_MARK:
+            return "Q_MARK";
+        case TokenType::LEFT_PAREN:
+            return "LEFT_PAREN";
+        case TokenType::RIGHT_PAREN:
+            return "RIGHT_PAREN";
+        case TokenType::COLON:
+            return "COLON";
+        case TokenType::COLON_DASH:
+            return "COLON_DASH";
+        case TokenType::MULTIPLY:
+            return "MULTIPLY";
+        case TokenType::ADD:
+            return "ADD";
+        case TokenType::SCHEMES:
+            return "SCHEMES";
+        case TokenType::FACTS:
+            return "FACTS";
+        case TokenType::RULES:
+            return "RULES";
+        case TokenType::QUERIES:
+            return "QUERIES";
+        case TokenType::ID:
+            return "ID";
+        case TokenType::STRING:
+            return "STRING";
+        case TokenType::COMMENT:
+            return "COMMENT";
+        case TokenType::END_OF_FILE:
+            return "EOF";
+        case TokenType::UNDEFINED:
+            return "UNDEFINED";
+        default:
+            return "(ERROR: UNDEFINED TOKEN TYPE STRING)";
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, TokenType& type) {
+    os << tokenTypeToString(type);
+    return os;
+}
+
 struct Token {
     TokenType type;
     std::string lexeme;
@@ -33,6 +82,17 @@ struct Token {
         type(type),
         lexeme(std::move(lexeme)),
         line(line) {}
+
+    std::string toString() const {
+        return
+        "(" +
+        tokenTypeToString(type) +
+        ",\"" +
+        lexeme +
+        "\"," +
+        std::to_string(line) +
+        ")";
+    }
 };
 
 inline bool operator==(const Token& rhs, const Token& lhs) {
@@ -40,6 +100,11 @@ inline bool operator==(const Token& rhs, const Token& lhs) {
     rhs.type == lhs.type &&
     rhs.lexeme == lhs.lexeme &&
     rhs.line == lhs.line;
+}
+
+std::ostream& operator<<(std::ostream& os, Token& token) {
+    os << token.toString();
+    return os;
 }
 
 #endif // TOKEN_H
